198273ec5a564e046a5354d3c1249752e405dc94
Mikolaj Figurski <meek.f80@gmail.com>
2023-03-14 17:15:51 -0400

    Add bucking cake

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+type JobAppStatus = 'applied' | 'interview' | 'offer' | 'rejected'
+
+type JobApplication = {
+  name: string
+  source: string
+  date: string
+  status: JobAppStatus 
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [stored, setStored] = useState<JobApplication[]>(
+    JSON.parse(localStorage.getItem('stored') || '[]'))
+
+  const [newInput, setNewInput] = useState<JobApplication>({
+    name: '',
+    source: '',
+    date: '',
+    status: 'applied'
+  })
+
+  useEffect(() => {
+    localStorage.setItem('stored', JSON.stringify(stored))
+  }, [stored])
+
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2 className="logo">🎂</h2> 
       </div>
-      <h1>Vite + React</h1>
+      <h1>Job App Register</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
