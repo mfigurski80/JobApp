64e8fa1ec0ca44dd38a4e99c84f14c82fcd2d9c8
Mikolaj Figurski <miko.f80@gmail.com>
2023-03-30 15:06:03 -0400

    fix: standardize emoji renders

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,13 +1,18 @@
+import { Twemoji } from 'react-emoji-render';
+
 import './Emoji.css'
 
-const EMOJI = "🎂 🐌 🐟 🐝 🕷️ 🦐 🐁 🦀 🐹 🐇 🐿️ 🐰 🦔 🐗 🐨 🐒 🦍 🦒 🦏 🐪 🐫 🦭 🐔 🐍 🦜 🦆 🦢 🦩 🦚 🐩 🐶 🐻 🐺 🦊 🦌 🦓 🐎 🦬 🦣 🐅 🐆 🦇 🦅 🦑 🦈 🐊 🦂 🐉 🐲 🦕 🦖 🐘 🐯 🦉 🦁 🦄 🐷".split(" ")
+const EMOJI = "🎂-🐌-🐟-🐝-🕷️-🦐-🐁-🦀-🐹-🐇-🐿️-🐰-🦔-🐗-🐨-🐒-🦍-🦒-🦏-🐪-🐫-🦭-🐔-🐍-🦜-🦆-🦢-🦩-🦚-🐩-🐶-🐻-🐺-🦊-🦌-🦓-🐎-🦬-🦣-🐅-🐆-🦇-🦅-🦑-🦈-🐊-🦂-🐉-🐲-🦕-🦖-🐘-🐯-🦉-🦁-🦄-🐷".split("-").filter(v => !!v)
 
 export default function Emoji({ count }: { count: number }) {
-  const em = count <= EMOJI.length ? EMOJI[count] : "⭐"
-
+  // const em = count < EMOJI.length ? EMOJI[count] : "⭐"
+  const em = EMOJI[count % EMOJI.length]
+  console.log(EMOJI)
   return (
     <div>
-      <h2 className="logo">{em}</h2>
+      <h2 className="logo">
+        <Twemoji text={em} />
+      </h2>
     </div>
   )
 }
